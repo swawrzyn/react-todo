5c1235318962bc0436df4a9b53416a79456e775d
Stefan Wawrzyn <swawrzyn@gmail.com>
2022-02-12 18:20:39 +0800

    fix.

--- src/components/ui/buttons/SimpleButton.tsx
+++ src/components/ui/buttons/SimpleButton.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default class SimpleButton extends React.Component {
   render() {
     return (
-      <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+      <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
         {this.props.children}
       </button>
     );
