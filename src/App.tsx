5e7b71d835b27d85f1a2cc104910eb6645524522
Stefan Wawrzyn <swawrzyn@gmail.com>
2022-02-12 17:56:28 +0800

    add packages: redux, react-router, storybook, and deploy to gh pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import NavBar from "./components/ui/nav/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/ui/buttons/SimpleButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  color: string;
+};
+export default class SimpleButton extends React.Component {
+  render() {
+    return (
+      <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        {this.props.children}
+      </button>
+    );
+  }
+}
